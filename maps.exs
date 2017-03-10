aaa256abefc1d12b603048de664fc44e53acca27
Manjunath Reddy <manjunath.chk@gmail.com>
2017-03-10 12:27:41 +0800

    Add more maps examples

--- maps.exs
+++ maps.exs
@@ -3,3 +3,7 @@ map = %{:a => 1, 2 => :b}
 IO.puts map[2]
 
 Enum.map(%{1 => 2, 3 => 4}, fn {k,v} -> IO.puts "key is #{k} and value is #{v}" end)
+
+# Ranges
+Enum.map(1..4, fn val -> val *2 end)
+# [2, 4, 6, 8]
